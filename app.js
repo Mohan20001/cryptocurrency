ca9260a747cc18dc5ebf0008f53cf440ce7ac196
mohan putta <puttamohan2001@gmail.com>
2021-11-10 17:19:00 +0530

    v0000

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+let url=`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true`;
 
-
-fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true`)
-.then(res => res.json())
+function fetchData(path) {
+    
+    fetch(path)
+    .then(res => res.json())
 .then(data => {
     console.log(data[0]);
      data.forEach(element => {
-        createComponent(element.market_cap_rank, element.name,Math.ceil(element.market_cap/1000000000), element.current_price,Math.floor(element.price_change_percentage_24h), element.image);
+        createComponent(element.market_cap_rank, element.name,strFormate(element.market_cap/1000000000), strFormate(element.current_price),strFormate(element.price_change_percentage_24h), element.image);
     });
 })
 .catch(err => console.log(err));
@@ -16,30 +18,30 @@ fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=mark
     
     let item=document.createElement('div');
     item.className="item";
-
+    
     let rank=document.createElement('div');
     rank.className="rank";
     rank.innerText=coinRank;
-      
+    
     let name=document.createElement('div');
     name.className="name";
     name.innerText=coinName;
-
+    
     let imag=document.createElement('img');
     imag.src=srcUrl;
     name.appendChild(imag);
 
     
     
-
+    
     let mcap=document.createElement('div');
     mcap.className="cap";
     mcap.innerText="$"+coinMcap+"B";
-
+    
     let price=document.createElement('div');
     price.className="price";
     price.innerText="$"+coinPrice;
-
+    
     let span=document.createElement('span');
     span.innerText=coinPercentage+"%";
    rankColor(span)
@@ -49,7 +51,7 @@ fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=mark
     item.appendChild(name);
     item.appendChild(mcap);
     item.appendChild(price);
-
+     
     ls.appendChild(item);
 }
 
@@ -58,5 +60,17 @@ function rankColor(str) {
     if (str.innerText.includes("-")) {
         str.style.color="red";
     }
- }
+    
+}
+    function strFormate(str) {
+        return (parseFloat(str)).toLocaleString(undefined, {minimumFractionDigits: 1});
+    }
+    
+alert(strFormate(8888.888888888));
 
+}
+if (navigator.onLine) {
+    fetchData(url);
+}else{
+    alert("offline");
+}
